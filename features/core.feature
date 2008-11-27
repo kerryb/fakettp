9b5b9a43ca8103b92a1f01581c09c34466d8ec1c
Kerry Buckley <kerryjbuckley@gmail.com>
2008-11-27 17:31:08 +0000

    Added some more feature tests.

--- features/core.feature
+++ features/core.feature
@@ -7,6 +7,30 @@ Feature: Mocking an HTTP server
     Given the simulator is reset
     And we expect get_root
     Then verifying the simulator should report a failure, with message "Expected request not received"
-  
 
-  
+  Scenario: One satisfied expectation
+    Given the simulator is reset
+    And we expect get_root
+    When we request /
+    Then verifying the simulator should report success
+
+  Scenario: Two satisfied expectations
+    Given the simulator is reset
+    And we expect get_root
+    And we expect get_foo
+    When we request /
+    And we request /foo
+    Then verifying the simulator should report success
+
+  Scenario: Two expectations, one satisfied
+    Given the simulator is reset
+    And we expect get_root
+    And we expect get_foo
+    When we request /
+    Then verifying the simulator should report a failure, with message "Expected request not received"
+
+  Scenario: Expectation failure
+    Given the simulator is reset
+    And we expect get_root
+    When we request /foo
+    Then verifying the simulator should report a failure, with message "Error in GET /: expected: "/",.*got: "/foo""
--- features/steps/core.rb
+++ features/steps/core.rb
@@ -1,24 +1,34 @@
+require 'httparty'
+
+class FakeTTP
+  include HTTParty
+  base_uri 'fakettp.local'
+end
+
 Given /^the simulator is reset$/ do
-  req = Net::HTTP::Post.new '/reset'
-  Net::HTTP.new('fakettp.local').start {|http| http.request(req) }
+  FakeTTP.post '/reset'
 end
 
 Given /^we expect (\S*)$/ do |filename|
-  req = Net::HTTP::Post.new '/expect', {'Content-Type' => 'text/plain'}
-  req.body = File.read(File.dirname(__FILE__) + "/../expectations/#{filename}")
-  Net::HTTP.new('fakettp.local').start {|http| http.request(req) }
+  body = File.read(File.dirname(__FILE__) + "/../expectations/#{filename}")
+  FakeTTP.post '/expect', :body => body, :headers => {'Content-Type' => 'text/plain'}
 end
 
 Then /^verifying the simulator should report success$/ do
   req = Net::HTTP::Get.new '/verify'
   resp = Net::HTTP.new('fakettp.local').start {|http| http.request(req) }
-  resp.class.should == Net::HTTPOK
   resp.body.should == "OK\n"
+  resp.class.should == Net::HTTPOK
 end
 
 Then /^verifying the simulator should report a failure, with message "(.*)"$/ do |message|
   req = Net::HTTP::Get.new '/verify'
   resp = Net::HTTP.new('fakettp.local').start {|http| http.request(req) }
+  resp.body.should =~ Regexp.new(message, Regexp::MULTILINE)
   resp.class.should == Net::HTTPBadRequest
-  resp.body.should =~ Regexp.new(message)
 end
+
+When /^we request (\S*)$/ do |path|
+  FakeTTP.get path rescue nil
+end
+
