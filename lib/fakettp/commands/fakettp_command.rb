3a04b7834b64ee81d151c074fe1a732d7c0335aa
Kerry Buckley <kerryjbuckley@gmail.com>
2009-02-11 09:12:43 +0000

    Moved guts of command into a class

---
+++ lib/fakettp/commands/fakettp_command.rb
@@ -0,0 +1,22 @@
+class FakettpCommand
+  def initialize args
+    @args = args
+  end
+  
+  def run
+    command = get_command
+    return usage unless command
+    return 0
+  end
+  
+  private
+  
+  def get_command
+    @args[0]
+  end
+  
+  def usage
+    STDERR.puts "No!"
+    return 1
+  end
+end
