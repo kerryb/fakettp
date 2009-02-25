16fb9db433bf19af0b8c747af8d19156316cd79d
Kerry Buckley <kerryjbuckley@gmail.com>
2009-02-25 21:50:19 +0000

    Expectation partially implemented

--- lib/fakettp/expectation.rb
+++ lib/fakettp/expectation.rb
@@ -1,4 +1,34 @@
 module Fakettp
   class Expectation
+    EXPECTATION_DIR = FAKETTP_BASE + '/expectations'
+    def self.clear_all
+      FileUtils.rm_rf Dir.glob(File.join(EXPECTATION_DIR, '*'))
+    end
+    
+    def self.<< expectation
+      File.open next_file_to_create, 'w' do |f|
+        f.write expectation
+      end
+    end
+    
+    def self.next
+      file = next_file_to_read
+      contents = File.read file
+      FileUtils.rm file
+      contents
+    end
+
+    
+    def self.next_file_to_create
+      name = (Dir.entries(EXPECTATION_DIR).last.to_i + 1).to_s
+      File.join EXPECTATION_DIR, name
+    end
+    
+    def self.next_file_to_read
+      name = Dir.entries(EXPECTATION_DIR)[2] # ignore . and ..
+      File.join EXPECTATION_DIR, name
+    end
+  
+    private_class_method :next_file_to_create, :next_file_to_read
   end
 end
--- spec/fakettp/expectation_spec.rb
+++ spec/fakettp/expectation_spec.rb
@@ -1,20 +1,78 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Fakettp::Expectation do
-  describe 'clearing all expectations' do
-    it 'should have behaviour'
+  before :all do
+    @expectation_dir = File.join FAKETTP_BASE, 'expectations'
+    FileUtils.mkdir_p @expectation_dir
   end
   
-  describe 'creating an expectation' do
-    it 'should have behaviour'
+  def setup_files *names
+    FileUtils.rm_rf Dir.glob(File.join(@expectation_dir, '*'))
+    names.each do |name|
+      FileUtils.touch File.join(@expectation_dir, name)
+    end
+  end
+  
+  describe 'clearing all expectations' do
+    before do
+      setup_files '1', '2'
+    end
+    
+    it 'should remove the contents of tmp/expectations' do
+      Fakettp::Expectation.clear_all
+      Dir.glob(File.join(@expectation_dir, '**', '*')).should be_empty
+    end
   end
   
   describe 'adding an expectation' do
-    it 'should have behaviour'
+    describe 'when there are existing expectations' do
+      before do
+        setup_files '1', '2'
+      end
+    
+      it 'should copy the supplied code to the next numbered expectation' do
+        expectation = "foo\nbar"
+        Fakettp::Expectation << expectation
+        File.read(File.join(@expectation_dir, '3')).should == expectation
+      end
+    end
+    
+    describe 'when there are no existing expectations' do
+      before do
+        setup_files
+      end
+
+      it 'should start at 1' do
+        expectation = "foo\nbar"
+        Fakettp::Expectation << expectation
+        File.read(File.join(@expectation_dir, '1')).should == expectation
+      end
+    end
   end
   
   describe 'getting the next expectation' do
-    it 'should have behaviour'
+    describe 'when there are remaining expectations' do
+      before do
+        setup_files '3', '4'
+        @expectation = "foo\nbar"
+        File.open File.join(@expectation_dir, '2'), 'w' do |f|
+          f.write @expectation
+        end
+      end
+  
+      it 'should return the contents of the first file' do
+        Fakettp::Expectation.next.should == @expectation
+      end
+    
+      it 'should delete the file' do
+        Fakettp::Expectation.next
+        File.exist?(File.join(@expectation_dir, '2')).should be_false
+      end
+    end
+
+    describe 'when there are remaining expectations' do
+      it 'should throw an exception or something'
+    end
   end
   
   describe 'executing' do
